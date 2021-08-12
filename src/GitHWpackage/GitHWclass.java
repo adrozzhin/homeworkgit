85524635d0e0537e47f9df79948b32a846d7adef
adrozzhin <drozzhinandrei@gmail.com>
2021-08-12 15:02:43 -0700

    print odd int from 1 to 100

--- src/GitHWpackage/GitHWclass.java
+++ src/GitHWpackage/GitHWclass.java
@@ -6,7 +6,10 @@ public class GitHWclass {
 		// TODO Auto-generated method stub
 		
 		for (int i=1;i<=100;i++) {
-			System.out.println(i);
+			if (i%2!=0) {
+				System.out.println(i);
+			}
+			
 		}
 
 	}
